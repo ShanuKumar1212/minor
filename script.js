61bc971eb666d5a6d89776d4f4e87c43dd6fc666
ShanuKumar1212 <146358411+ShanuKumar1212@users.noreply.github.com>
2024-02-01 08:56:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function chatbot()
 {
-    window.location.href = "https://mediafiles.botpress.cloud/548672e3-f625-432a-9c7b-d9d1def2096b/webchat/bot.html";
+    window.location.href = "https://mediafiles.botpress.cloud/fe43ef94-15d4-4ffe-a559-a6c96f4a44a9/webchat/bot.html";
 }
